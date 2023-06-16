0d0c2332c730d9d769923d2d70b31debe03ff2ef
oZerro <84384055+oZerro@users.noreply.github.com>
2023-06-16 12:16:02 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,15 +8,15 @@ from telegram.ext import Updater, CallbackContext, CommandHandler
 from telegram import Update
 
 
-def post_img(images_name, interval=60*60*4):
+def post_img(chat_id, images_name, interval=60*60*4):
     if images_name == None:
         while True:
             for img in list_img:
-                bot.send_photo(chat_id='@spaceozerro', photo=open(f'./images/{img}', 'rb'))
+                bot.send_photo(chat_id=chat_id, photo=open(f'./images/{img}', 'rb'))
                 time.sleep(interval)
             random.shuffle(list_img)
     else:
-        bot.send_photo(chat_id='@spaceozerro', photo=open(f'./images/{images_name}', 'rb'))
+        bot.send_photo(chat_id=chat_id, photo=open(f'./images/{images_name}', 'rb'))
 
 
 if __name__ == "__main__":
@@ -24,8 +24,9 @@ if __name__ == "__main__":
     bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
     list_img = list(os.walk("images"))[0][2]
     parser = argparse.ArgumentParser()
+    parser.add_argument("chat_id")
     parser.add_argument("--interval", type=int)
     parser.add_argument("--images_path", type=str)
     args = parser.parse_args()
-    post_img(args.images_path, args.interval)
+    post_img(args.chat_id, args.images_path, args.interval)
 
