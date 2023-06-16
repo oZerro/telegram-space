9d5b03b933dfe9b652e3dbc238e0ab1feb5d5a0e
oZerro <leha.normatov@mail.ru>
2023-06-16 10:42:51 +0300

    add_argpars

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,12 +8,15 @@ from telegram.ext import Updater, CallbackContext, CommandHandler
 from telegram import Update
 
 
-def post_img(interval=60*60*4):
-    while True:
-        for img in list_img:
-            bot.send_photo(chat_id='@spaceozerro', photo=open(f'./images/{img}', 'rb'))
-            time.sleep(interval)
-        random.shuffle(list_img)
+def post_img(images_name, interval=60*60*4):
+    if images_name == None:
+        while True:
+            for img in list_img:
+                bot.send_photo(chat_id='@spaceozerro', photo=open(f'./images/{img}', 'rb'))
+                time.sleep(interval)
+            random.shuffle(list_img)
+    else:
+        bot.send_photo(chat_id='@spaceozerro', photo=open(f'./images/{images_name}', 'rb'))
 
 
 if __name__ == "__main__":
@@ -22,6 +25,7 @@ if __name__ == "__main__":
     list_img = list(os.walk("images"))[0][2]
     parser = argparse.ArgumentParser()
     parser.add_argument("--interval", type=int)
+    parser.add_argument("--images_path", type=str)
     args = parser.parse_args()
-    post_img(args.interval)
+    post_img(args.images_path, args.interval)
 
