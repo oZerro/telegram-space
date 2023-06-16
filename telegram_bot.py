caefd084cf12c7fddaa3f8327ce7334cc3e8ebdf
oZerro <84384055+oZerro@users.noreply.github.com>
2023-06-16 12:38:09 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,16 +8,20 @@ from telegram.ext import Updater, CallbackContext, CommandHandler
 from telegram import Update
 
 
-def post_img(chat_id, images_name, interval=60*60*4):
-    if images_name == None:
-        while True:
+def infiniti_post(chat_id, list_img, interval):
+    while True:
             for img in list_img:
                 bot.send_photo(chat_id=chat_id, photo=open(f'./images/{img}', 'rb'))
                 time.sleep(interval)
             random.shuffle(list_img)
+
+def post_img(chat_id, images_name, interval=60*60*4):
+    if images_name == None:
+        infiniti_post(chat_id, list_img, interval)
     else:
         bot.send_photo(chat_id=chat_id, photo=open(f'./images/{images_name}', 'rb'))
-
+        infiniti_post(chat_id, list_img, interval)
+        
 
 if __name__ == "__main__":
     load_dotenv()
