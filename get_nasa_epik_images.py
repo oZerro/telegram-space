2ce2ccdb1c782891795a2dbef28ddfceb9770b95
oZerro <leha.normatov@mail.ru>
2023-06-23 18:25:23 +0300

    Added the default value "args" to the argument
    Added the default value "args" to the argument
    Added a condition for exiting the loop

--- get_nasa_epik_images.py
+++ get_nasa_epik_images.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-def get_nasa_epik(token, count=10):
+def get_nasa_epik(token, count):
     params = {
         "api_key": f"{token}"
     }
@@ -18,13 +18,15 @@ def get_nasa_epik(token, count=10):
     for image_number, img in enumerate(response.json()):
         date_split = img['date'].split(" ")[0].split("-")
         year, month, day = tuple(date_split)
-
         date = datetime.datetime(year=int(year), month=int(month), day=int(day))
         formatted_date = date.strftime("%Y/%m/%d")
         archive_url = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{img['image']}.png"
         file_format = get_file_extension(archive_url)
         save_img(archive_url, params, f'nasa_epik_{image_number}{file_format}')
-        
+
+        count -= 1
+        if count == 0:
+            break        
 
 
 if __name__ == "__main__":
@@ -37,6 +39,9 @@ if __name__ == "__main__":
                      По дефолту скачивает 10 изображений. Если вам нужно определенное количество,\
                      то можете передать значание в аргуметы при запуске.'
     )
-    parser.add_argument("--count", type=int, help="Количество изображений для скачивания")
+    parser.add_argument("--count", 
+                        type=int, 
+                        default=10,
+                        help="Количество изображений для скачивания")
     args = parser.parse_args()
     get_nasa_epik(token, args.count)
