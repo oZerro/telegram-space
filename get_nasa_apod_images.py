bdcf6902cca723bf2abe3f980fbc1635109f4173
oZerro <leha.normatov@mail.ru>
2023-06-23 18:10:48 +0300

    Removed an extra condition
    Removed an extra condition
    Added the default value "args" to the argument

--- get_nasa_apod_images.py
+++ get_nasa_apod_images.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-def get_nasa_apod(token, count=30):
+def get_nasa_apod(token, count):
     params = {
         "api_key": f"{token}",
         "count": count
@@ -15,11 +15,7 @@ def get_nasa_apod(token, count=30):
     response = requests.get(url, params=params)
     response.raise_for_status()
     
-    dictionary_with_images = []
     response = response.json()
-    if type(response) == dict:
-        dictionary_with_images.append(response)
-        response = dictionary_with_images
 
     for image_number, link_for_image in enumerate(response):
         file_format = get_file_extension(link_for_image['url'])
@@ -37,6 +33,9 @@ if __name__ == "__main__":
                     По дефолту скачивает 30 изображений.\n Если вам нужно определенное количество,\
                     то можете передать значание в аргумете COUNT при запуске. '
     )
-    parser.add_argument("--count", type=int, help="Количество изображений для скачивания")
+    parser.add_argument("--count",
+                        type=int,
+                        default=30, 
+                        help="Количество изображений для скачивания")
     args = parser.parse_args()
     get_nasa_apod(token, args.count)
